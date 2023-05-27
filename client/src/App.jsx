e1d0d4a16f3ecb48f55fe04cdc124c0841fb42ce
Anmol Porwal <anmolg99118685@gmail.com>
2023-05-27 18:11:43 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,8 +16,8 @@ import PlacePage from './pages/PlacePage'
 import BookingPage from './pages/BookingPage'
 import BookingsPage from './pages/BookingsPage'
 
-
-axios.defaults.baseURL='http://localhost:4000';
+// import.meta.env.VITE_API_BASE_URL
+axios.defaults.baseURL=import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials=true
 function App() {
 
